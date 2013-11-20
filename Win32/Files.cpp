b1df3062ef426f180266f2e7ca88654037b36863
Florent Espanet <florent.esp@gmail.com>
2013-11-20 11:55:04 +0100

    first try of windows version

--- Win32/Files.cpp
+++ Win32/Files.cpp
@@ -1,19 +1,39 @@
 #if defined(_WIN32) || defined(__WIN32__)
 
-#include <unistd.h>
+#include <cstring>
+#include <cstddef>
 #include <windows.h>
 #include "Files.hpp"
 
-void	*Files::open(const char *filename)
+void	*Files::first(const char *dirname, char **filename)
 {
+  void			*dir = NULL;
+  WIN32_FIND_DATA	handle = NULL;
+
+  if ((dir = FindFirstFile(dirname, &handle)) == INVALID_HANDLE_VALUE)
+    return (NULL);
+  *filename = new char[256];
+  memcpy(*filename, handle.cFileName, 256);
+  return (dir);
 }
 
-void	*Files::next(void *handle, const char *sym)
+char *Files::next(void *dir)
 {
+  char			*filename = new char[256];
+  WIN32_FIND_DATA	data = NULL;
+
+  if (FindNextFile(dirname, &handle) == 0)
+    return (NULL);
+  memcpy(filename, handle.cFileName, 256);
+  return (filename);
 }
 
-bool	Files::close(void *handle)
+bool	Files::close(void *dir)
 {
+  bool	closed = false;
+
+  closed = (FindFree(dir) != 0);
+  return (closed);
 }
 
 #endif
